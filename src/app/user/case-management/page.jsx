4d9421b8cb8b181523d8fe4d6c169613189e01c5
junaidsalam495 <junaidsalam495@gmail.com>
2025-07-14 05:51:25 +0500

    api integreration and both panel api integrate

--- src/app/user/case-management/page.jsx
+++ src/app/user/case-management/page.jsx
@@ -6,6 +6,15 @@ import CaseManagementCard from '@/components/(user)/case-management/case-managem
 async function CaseManagementPage() {
     const responseUsername = await fetch(`${basedUrl}/usernames`);
     const dataUsername = await responseUsername.json();
+
+    if (!dataUsername || dataUsername?.length === 0) return (
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="text-2xl font-bold text-gray-800 dark:text-white">
+                No case available
+            </h1>
+        </div>
+    )
+
     return (
         <>
             <DashboardProvider>
--- src/app/user/log-management/page.jsx
+++ src/app/user/log-management/page.jsx
@@ -8,6 +8,14 @@ async function LogManagement() {
     const response = await fetch(`${basedUrl}/logs`);
     const data = await response.json();
 
+    if (!data || data?.length === 0) return (
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="text-2xl font-bold text-gray-800 dark:text-white">
+                No logs available
+            </h1>
+        </div>
+    )
+
     return (
         <>
             <DashboardProvider>
