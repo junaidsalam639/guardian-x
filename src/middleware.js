3837c2aeb7d21784e4615ebac314288d59b059cd
junaidhnhtechsolutions <junaidhnhtechsolutions@gmail.com>
2025-07-14 17:07:47 +0500

    middle ware

--- src/middleware.js
+++ src/middleware.js
@@ -7,12 +7,13 @@ export function middleware(request) {
   const token = request.cookies.get("token")?.value;
 
   const isPublic = publicRoutes.includes(pathname);
+  const redirectUrl = pathname.startsWith("/user") ? "/user/dashboard" : "/admin/client-management";
 
   if (!token && !isPublic && (pathname.startsWith("/user") || pathname.startsWith("/admin"))) {
     return NextResponse.redirect(new URL("/", request.url));
   }
-  // if (token && (pathname === "/" || pathname === "/admin/login")) {
-  //   return NextResponse.redirect(new URL("/user/dashboard", request.url));
-  // }
+  if (token && (pathname === "/" || pathname === "/admin/login")) {
+    return NextResponse.redirect(new URL(redirectUrl, request.url));
+  }
   return NextResponse.next();
 }
