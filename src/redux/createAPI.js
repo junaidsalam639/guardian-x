edb710d729210baaaf0a92fdf07ad05137007efc
junaidsalam495 <junaidsalam495@gmail.com>
2025-08-11 00:25:57 +0500

    changes based url

--- src/redux/createAPI.js
+++ src/redux/createAPI.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const createAPI = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://app.xylox.ai/',
+        baseUrl: 'https://app.xylox.ai/api/',
         prepareHeaders: (headers, { getState }) => {
             const token = getState().auth.token;
             headers.set("Content-Type", "application/json");
